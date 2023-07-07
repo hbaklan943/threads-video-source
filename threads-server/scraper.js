64440285fbfbd543cf4ba4eadf01d758b8c8bfc8
hbaklan943 <hbaklan943@gmail.com>
2023-07-07 18:21:42 +0300

    update args and executable path

--- threads-server/scraper.js
+++ threads-server/scraper.js
@@ -5,11 +5,23 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static('dist'))
 const puppeteer = require('puppeteer')
+require("dotenv").config();
 
 
 const getVideo = async url => {
   console.log(url);
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch({
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
+    executablePath: 
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+  });
   const page = await browser.newPage();
 
   await page.goto(url)
